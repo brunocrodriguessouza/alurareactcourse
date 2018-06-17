da2a4d46e0f5932289e789aea0aa67517ea63d55
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-06-17 04:20:34 -0300

    Validacao dos inputs obrigatorios e limpar quando for enviado novamente usando novamente o padrao Observer

--- src/Autor.js
+++ src/Autor.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
 import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 import PubSub from 'pubsub-js';
+import TratadorErros from './TratadorErros';
 
 class FormularioAutor extends Component {
 
@@ -28,9 +29,14 @@ class FormularioAutor extends Component {
         data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
         success: function(novaListagem){
             PubSub.publish('atualizar-lista-autores',novaListagem);
-        },
+            this.setState({nome:'',email:'', senha:''})
+        }.bind(this),
         error: function(resposta) {
-            console.log("erro");
+            if (resposta.status === 400) {
+                new TratadorErros().publicarErros(resposta.responseJSON);
+            }
+        },beforeSend: function() {
+            
         }
         });
     }
