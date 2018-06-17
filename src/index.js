cf19bc9de43efb154bef0124c00d63faeaab05b2
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-06-17 20:23:46 -0300

    Configuracao React router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 
-import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/autor" />
+      <Route path="/livro" />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
