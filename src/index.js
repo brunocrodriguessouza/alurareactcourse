c30cf2a15a57f5c791b609bdf18fe98214a1dc5d
Bruno Cicero Rodrigues de Souza <bruno.rodrigues@venturus.org.br>
2018-06-18 08:09:14 -0300

    Navegando com history API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorBox from './Autor';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 
@@ -8,7 +9,7 @@ ReactDOM.render(
   <Router>
     <div>
       <Route exact path="/" component={App} />
-      <Route path="/autor" />
+      <Route path="/autor" component={AutorBox}/>
       <Route path="/livro" />
     </div>
   </Router>,
